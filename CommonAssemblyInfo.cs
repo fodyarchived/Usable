f7619cc53f7ecd8be4810c5a8425f565971dc72a
Cameron MacFarland <distantcam@gmail.com>
2013-07-07 15:34:38 +0800

    Version bump

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Usable")]
 [assembly: AssemblyProduct("Usable")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
